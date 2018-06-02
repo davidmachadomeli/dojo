add160c6077b14ce242cbc04ff95648799716bf1
David Machado <david.machado@mercadolibre.com>
2018-06-02 10:54:03 -0300

    add test for case when we are requesting a classroom for at least 10 persons

--- dojo_12/java/src/test/java/DojoTest.java
+++ dojo_12/java/src/test/java/DojoTest.java
@@ -1,22 +1,45 @@
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import com.mercadolibre.dojo.*;
+import com.mercadolibre.dojo.classrooms.Classroom;
+import com.mercadolibre.dojo.classrooms.IClassroom;
+import com.mercadolibre.dojo.matchers.CapacityOfAtLeast;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-/**
- * Tests for the dojo.
- */
-public class DojoTest {
-	
-	@Before
-	public void setup() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-	}
+class DojoTest {
 
-	@Test
-	public void test_ejemplo() {
+    private ClassroomsPool pool;
 
-	    Assert.assertTrue(true);
-	    Assert.assertEquals(new B(), new B());
-	}
+    private Classroom labA;
+    private Classroom labB;
+
+    @BeforeEach
+    void setup() {
+        this.labA = new Classroom(new Label("Lab A"), new Persons(5));
+        this.labB = new Classroom(new Label("Lab B"), new Persons(15));
+
+        this.pool = new ClassroomsPool(this.labB, this.labA);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.labA = null;
+        this.labB = null;
+
+        this.pool = null;
+    }
+
+    @Test
+    void search_classroom_for_at_least_10_persons_should_return_labB() {
+        // GIVEN
+        final IClassroom classroomWithMatchingRequisition = this.pool.searchClassroomFor(
+                new Requisition(new CapacityOfAtLeast(new Persons(10)))
+        );
+
+        // EXPECT
+        assertEquals(this.labB, classroomWithMatchingRequisition);
+    }
 
 }
