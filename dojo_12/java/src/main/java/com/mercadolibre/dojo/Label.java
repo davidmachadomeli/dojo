9d7317dfedb5c16e6c492a3956b6433c63636abf
David Machado <david.machado@mercadolibre.com>
2018-06-02 12:39:58 -0300

    adding code to complete the second test, we only need to override equals to make all no classroom equals

--- dojo_12/java/src/main/java/com/mercadolibre/dojo/Label.java
+++ dojo_12/java/src/main/java/com/mercadolibre/dojo/Label.java
@@ -1,11 +1,24 @@
 package com.mercadolibre.dojo;
 
+import java.util.Objects;
+
 public class Label {
 
     private String labelName;
 
     public Label(String labelName) {
         this.labelName = labelName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Label)) {
+            return false;
+        }
+
+        final Label label = (Label) o;
+        return Objects.equals(labelName, label.labelName);
     }
 
 }
--- dojo_12/java/src/main/java/com/mercadolibre/dojo/classrooms/NoClassroom.java
+++ dojo_12/java/src/main/java/com/mercadolibre/dojo/classrooms/NoClassroom.java
@@ -1,7 +1,8 @@
 package com.mercadolibre.dojo.classrooms;
 
 import com.mercadolibre.dojo.Label;
-import com.mercadolibre.dojo.matchers.Matcher;
+
+import java.util.Objects;
 
 public class NoClassroom implements IClassroom {
 
@@ -15,4 +16,16 @@ public class NoClassroom implements IClassroom {
     public IClassroom challenge(IClassroom challengerObject) {
         return challengerObject;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof NoClassroom)) {
+            return false;
+        }
+
+        final NoClassroom noClassroom = (NoClassroom) o;
+        return Objects.equals(label, noClassroom.label);
+    }
+
 }
