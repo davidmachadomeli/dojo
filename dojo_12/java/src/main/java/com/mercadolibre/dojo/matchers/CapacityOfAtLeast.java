955cf58bff6d6c5aee7a05ada0850e7a08d75727
David Machado <david.machado@mercadolibre.com>
2018-06-03 19:26:00 -0300

    add needed classes and methods to make the previous test pass

--- dojo_12/java/src/main/java/com/mercadolibre/dojo/matchers/CapacityOfAtLeast.java
+++ dojo_12/java/src/main/java/com/mercadolibre/dojo/matchers/CapacityOfAtLeast.java
@@ -1,10 +1,27 @@
 package com.mercadolibre.dojo.matchers;
 
 import com.mercadolibre.dojo.Persons;
+import com.mercadolibre.dojo.classrooms.Classroom;
+import com.mercadolibre.dojo.classrooms.IClassroom;
 
-public class CapacityOfAtLeast {
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class CapacityOfAtLeast implements Matcher {
+
+    private Persons persons;
 
     public CapacityOfAtLeast(Persons persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public List<IClassroom> returnListOfMatchingClassrooms(List<Classroom> classrooms) {
+        return classrooms
+                .stream()
+                .map(classroom -> classroom.returnIfHasAtLeastAsManyPersons(this.persons))
+                .collect(toList());
     }
 
 }
