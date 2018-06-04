23d3cbd0180943c3fe3cc3f76e25cc538b301442
David Machado <david.machado@mercadolibre.com>
2018-06-03 22:23:03 -0300

    add needed classes and methods to make the previous test pass

---
+++ dojo_12/java/src/main/java/com/mercadolibre/dojo/matchers/CapacityOfLessThan.java
@@ -0,0 +1,27 @@
+package com.mercadolibre.dojo.matchers;
+
+import com.mercadolibre.dojo.classrooms.Classroom;
+import com.mercadolibre.dojo.classrooms.IClassroom;
+import com.mercadolibre.dojo.matchers.operations.LT;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class CapacityOfLessThan implements Matcher {
+
+    private LT operator;
+
+    public CapacityOfLessThan(LT operator) {
+        this.operator = operator;
+    }
+
+    @Override
+    public List<IClassroom> returnListOfMatchingClassrooms(List<Classroom> classrooms) {
+        return classrooms
+                .stream()
+                .map(classroom -> classroom.returnIfMatchesCondition(this.operator))
+                .collect(toList());
+    }
+
+}
