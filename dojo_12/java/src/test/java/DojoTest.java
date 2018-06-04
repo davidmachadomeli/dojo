91c3456a49e7ee2678123ce8ba0fd463c66c91c1
David Machado <david.machado@mercadolibre.com>
2018-06-03 21:09:50 -0300

    add third test where I ask for a classroom for more than 10 persons and more than 30 square meters and get lab C since it pass all the requirements

--- dojo_12/java/src/test/java/DojoTest.java
+++ dojo_12/java/src/test/java/DojoTest.java
@@ -1,10 +1,12 @@
 import com.mercadolibre.dojo.ClassroomsPool;
 import com.mercadolibre.dojo.Label;
-import com.mercadolibre.dojo.Persons;
 import com.mercadolibre.dojo.RequestForClassroom;
 import com.mercadolibre.dojo.classrooms.Classroom;
 import com.mercadolibre.dojo.classrooms.IClassroom;
 import com.mercadolibre.dojo.classrooms.NoClassroom;
+import com.mercadolibre.dojo.classrooms.specs.Persons;
+import com.mercadolibre.dojo.classrooms.specs.SquareMeters;
+import com.mercadolibre.dojo.matchers.And;
 import com.mercadolibre.dojo.matchers.CapacityOfAtLeast;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,13 +20,27 @@ class DojoTest {
 
     private Classroom labA;
     private Classroom labB;
+    private Classroom labC;
 
     @BeforeEach
     void setup() {
-        this.labA = new Classroom(new Label("Lab A"), new Persons(5));
-        this.labB = new Classroom(new Label("Lab B"), new Persons(15));
+        this.labA = new Classroom(
+                new Label("Lab A"),
+                new Persons(5),
+                new SquareMeters(10)
+        );
+        this.labB = new Classroom(
+                new Label("Lab B"),
+                new Persons(15),
+                new SquareMeters(20)
+        );
+        this.labC = new Classroom(
+                new Label("Lab C"),
+                new Persons(15),
+                new SquareMeters(50)
+        );
 
-        this.pool = new ClassroomsPool(this.labA, this.labB);
+        this.pool = new ClassroomsPool(this.labA, this.labB, this.labC);
     }
 
     @AfterEach
@@ -55,6 +71,22 @@ class DojoTest {
 
         // EXPECT
         assertEquals(this.labB, classroomWithMatchingRequisition);
+    }
+
+    @Test
+    void search_classroom_for_at_least_10_persons_and_30_square_meters_should_return_labC() {
+        // GIVEN
+        final IClassroom classroomWithMatchingRequisition = this.pool.searchClassroomFor(
+                new RequestForClassroom(
+                        new And(
+                                new CapacityOfAtLeast(new Persons(10)),
+                                new CapacityOfAtLeast(new SquareMeters(30))
+                        )
+                )
+        );
+
+        // EXPECT
+        assertEquals(this.labC, classroomWithMatchingRequisition);
     }
 
 }
